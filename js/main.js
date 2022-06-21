9ee62636aaeb6915eadffd9435ff6d393235eb5c
siobhankb <siobhan.boylan@gmail.com>
2022-06-21 02:59:54 -0500

    try to refresh instead of append

--- js/main.js
+++ js/main.js
@@ -16,22 +16,19 @@ let cityInput = document.getElementById("cityInputForm");
 
   // handleEvent --> first set function for click
   async function handleSubmit(event) {
-    console.log(event.target);
     event.preventDefault();
     let userCity = event.target.city.value;
-    console.log(userCity);
     let w = await getWeatherData(userCity);
     // get info from input, send to display output
     displayWeatherCityInfo(w);
-    event.target.value = "";
+    event.target.city.value = "";
   }
 
   //addEventListener
   cityInput.addEventListener("submit", handleSubmit);
-  console.log(cityInput);
 
   // get API data
-
+  const key = "8f519a092dcc33fa81b709ac8021deb1";
   async function getWeatherData(city) {
     let weatherCityAPIurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=imperial`;
     console.log(weatherCityAPIurl);
@@ -57,9 +54,7 @@ let cityInput = document.getElementById("cityInputForm");
     // get div to display info:
     const weatherDataDisplay = document.getElementById("current");
 
-    let content = "";
-
-    //make current conditions card:
+    //display current conditions card:
     const currRow = document.createElement("div");
     currRow.className = "row justify-content-md-center";
     weatherDataDisplay.append(currRow);
@@ -67,11 +62,13 @@ let cityInput = document.getElementById("cityInputForm");
     currCol.className = "col col-md-auto text-center text-middle";
     currRow.append(currCol);
     const currCard = document.createElement("div");
-    currCard.className = "card";
+    currCard.className = "card border-0";
+    currCard.id = "current-conditions"
+    currCol.append(currCard);
     const currCardBody = document.createElement("div");
     currCardBody.className = "card-body fw-bold fs-2";
     currCardBody.innerHTML = `Currently ${currentTemp}&deg; and ${condition} in ${weatherCityObj["name"]}`;
-    currCol.append(currCardBody);
+    currCard.append(currCardBody);
     const iconCol = document.createElement("div");
     iconCol.className = "col col-1";
     currRow.append(iconCol);
@@ -84,6 +81,8 @@ let cityInput = document.getElementById("cityInputForm");
       ["High:", maxTemp],
       ["Low:", minTemp],
     ];
+
+    let content = "";
 
     weather.forEach((w) => {
       content += `
@@ -146,20 +145,3 @@ let cityInput = document.getElementById("cityInputForm");
   }
   // end of module
 }
-
-// console.log(weatherCityObj);
-// let temp = weatherCityObj["main"]["temp"];
-// //
-// console.log();
-// console.log(temp);
-// let maxiTemp = weatherCityObj["main"]["temp_max"];
-// console.log(maxiTemp);
-// let miniTemp = weatherCityObj["main"]["temp_min"];
-// console.log(miniTemp);
-// let feelLike = weatherCityObj["main"]["feels_like"];
-// console.log(feelLike);
-
-// let conditions = weatherCityObj["weather"][0]["main"];
-// console.log(conditions);
-// let icon0 = weatherCityObj["weather"][0]["icon"];
-// console.log(icon0);
